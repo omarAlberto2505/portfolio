9950cdd6c07a7da019ec195c40e5b49a6e4c83eb
OmarMurillo <64609112+OmarMurillo@users.noreply.github.com>
2026-07-28 17:52:13 -0600

    fix: Wrong deploy in github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-    base: 'https://omaralberto2505.github.io/portfolio/', // Link page
+    base: 'portfolio/', // Link page
 
 });
