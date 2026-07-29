1d2fe7a2d884f1190a1a2f7fd759a7c687035879
OmarMurillo <64609112+OmarMurillo@users.noreply.github.com>
2026-07-28 18:18:33 -0600

    fix: Wrong base in vitest

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-    base: 'portfolio/', // Link page
+    base: 'https://omaralberto2505.github.io/portfolio/', // Link page
 
 });
